8a362d8da85e9a48e2a42eb1209b37d7a6264c1d
Kai Willadsen <kai.willadsen@gmail.com>
2015-01-25 09:40:38 +1000

    When caching remote albums, only re-fetch changed ones

--- mirrormug.py
+++ mirrormug.py
@@ -254,7 +254,17 @@ def getalbums():
 
 @cli.command()
 def cachealbumsremote():
-    album_cache = smugmug.albums_get(NickName=NICKNAME)
+
+    try:
+        with open(CACHE_PATH) as f:
+            old_cache = simplejson.load(f)
+    except Exception:
+        old_cache = {}
+
+    old_albums = {
+        album['id']: album for album in old_cache['albums']['Albums']}
+
+    album_cache = smugmug.albums_get(NickName=NICKNAME, Heavy=True)
     image_cache = {}
     metadata_cache = {
         'albums': album_cache,
@@ -263,12 +273,21 @@ def cachealbumsremote():
     with click.progressbar(
             album_cache["Albums"], label='Caching albums') as albums:
         for album in albums:
-            images = smugmug.images_get(
-                AlbumID=album['id'], AlbumKey=album['Key'], Heavy=True)
+            old_album = old_albums.get(album['id'], {})
+            old_updated = old_album.get('LastUpdated', '')
+            updated = album['LastUpdated']
+
+            if updated == old_updated:
+                images = old_cache['images'][str(album['id'])]
+            else:
+                images = smugmug.images_get(
+                    AlbumID=album['id'], AlbumKey=album['Key'], Heavy=True)
             image_cache[album['id']] = images
 
     with open(CACHE_PATH, 'w') as f:
         simplejson.dump(metadata_cache, f, indent=2)
+
+    return metadata_cache
 
 
 def get_local_md5sums():
